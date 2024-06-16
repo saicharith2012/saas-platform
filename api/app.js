46e972844dd96d41a410b9eca68aaf8d9899d7c3
saicharith2012 <saicharithpalabindhela@gmail.com>
2024-06-16 23:01:00 +0530

    resolving cors error and adding routes

--- api/app.js
+++ api/app.js
@@ -7,8 +7,9 @@ const app = express();
 // cors
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
+    origin: "http://localhost:3000",  // <-- location of the react app
+    methods: ["GET", "POST", "PUT", "DELETE"], 
+    allowedHeaders: ["Content-Type", "Authorization"]
   })
 );
 
@@ -30,10 +31,18 @@ app.use(cookieParser())
 import planRouter from "./routes/plan.routes.js"
 import userRouter from "./routes/user.routes.js"
 import organizationRouter from "./routes/organization.routes.js"
+import paymentRouter from "./routes/payment.routes.js"
+import productRouter from "./routes/product.routes.js"
+import orderRouter from "./routes/order.routes.js"
 
 app.use("/api/plans", planRouter)
 app.use("/api/users", userRouter)
 app.use("/api/organizations", organizationRouter)
+app.use("/api/payments", paymentRouter)
+app.use("/api/products", productRouter)
+app.use("/api/orders", orderRouter)
 
+// webhook for stripe payments
 
 export default app;
+
