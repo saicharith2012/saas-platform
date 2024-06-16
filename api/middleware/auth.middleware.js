c2ddbb0f7ae48cdf8088236cb586410c36c3528b
saicharith2012 <saicharithpalabindhela@gmail.com>
2024-06-16 07:40:26 +0530

    authentication and authorization middleware

---
+++ api/middleware/auth.middleware.js
@@ -0,0 +1,42 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.models.js";
+
+// middleware to verify the jwt token and add the user to the request object.
+const verifyJWT = async (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization").replace("Bearer ", "");
+    if (!token) {
+      return res.status(401).send({ error: "Unauthorized Request." });
+    }
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findById(decodedToken._id).select(
+      "-password -refreshToken"
+    );
+    if (!user) {
+      return res.status(401).send({ error: "Invalid Access Token." });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (err) {
+    res
+      .status(403)
+      .send({ error: err?.message, message: "Please Authenticate." });
+  }
+};
+
+// middleware to verify token and authorize specific roles.
+const verifyAuthorization = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).send({ error: "Unauthroized Request." });
+    }
+
+    next();
+  };
+};
+
+export { verifyJWT, verifyAuthorization };
