a1d2d001c2db5ba4ce3e04afc814ab7363b371c3
saicharith2012 <saicharithpalabindhela@gmail.com>
2024-06-16 23:02:16 +0530

    order and product models

---
+++ api/models/order.models.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          default: 1,
+        },
+      },
+    ],
+    orderStatus: {
+      type: String,
+      enum: ["failed", "success"],
+      default: "failed",
+    },
+    paymentId: {
+      type: String,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Order = mongoose.model("Order", orderSchema);
---
+++ api/models/product.models.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    imageLink: {
+      String,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Product = mongoose.model("Product", productSchema);
