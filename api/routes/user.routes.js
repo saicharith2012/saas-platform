fb7f38bc112d73e1bbe1f4881909d9180982209b
saicharith2012 <saicharithpalabindhela@gmail.com>
2024-06-17 21:11:32 +0530

    get user details route

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -35,6 +35,16 @@ router
 // any user login -> user privilege
 router.route("/login").post(loginUser);
 
+// user profile route
+router.route("/profile").get(verifyJWT, async (req, res) => {
+  try {
+    const user = req.user;
+    res.status(200).json({ user });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 // log out -> user privilege
 router.route("/logout").post(verifyJWT, logoutUser);
 
@@ -42,12 +52,12 @@ router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/change-password").put(verifyJWT, changePassword);
 
 // add product to cart -> user privilege
-router.route("/:userId/add-to-cart").post(verifyJWT, addProductToCart)
+router.route("/:userId/add-to-cart").post(verifyJWT, addProductToCart);
 
 // get user's cart -> user privilege
-router.route("/:userId/cart").get(verifyJWT, getUserCart)
+router.route("/:userId/cart").get(verifyJWT, getUserCart);
 
 // get order history of the user
-router.route("/:userId/orders").get(verifyJWT, getOrderHistory)
+router.route("/:userId/orders").get(verifyJWT, getOrderHistory);
 
 export default router;
