3654a3f9ef90bd7ff1de6f5d9a39ee2f3afff075
saicharith2012 <saicharithpalabindhela@gmail.com>
2024-06-17 21:12:42 +0530

    routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,41 @@
-import './App.css';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Plans from './pages/Plans';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Plans from "./pages/Plans";
+import LoginForm from "./components/LoginForm.jsx";
+import SuperAdminDashboard from "./pages/SuperAdminDashboard.jsx";
+import AdminDashboard from "./pages/AdminDashboard.jsx";
+import Home from "./pages/Home.jsx";
+import Products from "./components/Product.jsx";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { loadUserFromToken } from "./authSlice.js";
 
 function App() {
+  const dispatch = useDispatch()
+  const { user } = useSelector((state) => state.auth)
+
+  useEffect(() => {
+    if(!user) {
+      dispatch(loadUserFromToken())
+    }
+  }, [dispatch, user])
+
+
   return (
-    <div className="App">
     <Router>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/plans" element={<Plans/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/plans" element={<Plans />} />
+        <Route path="/superadmin-dashboard" element={<SuperAdminDashboard />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        <Route path="/products" element={<Products />} />
       </Routes>
     </Router>
-    </div>
   );
 }
 
