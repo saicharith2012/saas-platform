b725f182641b813523fb72eae23904d5a536b2f7
saicharith2012 <saicharithpalabindhela@gmail.com>
2024-06-23 20:29:09 +0530

    bug fix: user limit

--- client/src/pages/SuperAdminDashboard.jsx
+++ client/src/pages/SuperAdminDashboard.jsx
@@ -289,7 +289,7 @@ function SuperAdminDashboard() {
                   <td>{plan.name}</td>
                   <td>{plan.description}</td>
                   <td>{plan.pricePerUserPerYear}</td>
-                  <td>{!plan.userLimit ? "NA" : plan.userLimit}</td>
+                  <td>{!plan.userLimit ? "NA" : plan.userLimit - 1}</td>
                   <td>
                     <button onClick={() => openEditModal(plan)}>Edit</button>
                     <button onClick={() => handleDeletePlan(plan._id)}>
